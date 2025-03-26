11211e2cd94dc225b62b6fc8b3627e7acb1c74e6
Martien Dermawan Tanama <martiendt@gmail.com>
2025-03-26 08:12:11 +0700

    x

--- src/modules/documents/validations/create.validation.ts
+++ src/modules/documents/validations/create.validation.ts
@@ -1,3 +1,8 @@
 export const createValidation = {
+  code: ['required', 'string'],
   name: ['required', 'string'],
+  type: ['required', 'string'],
+  owner: ['required'],
+  vault: ['required'],
+  rack: ['required', 'string'],
 }
