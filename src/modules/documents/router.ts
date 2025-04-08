7370ca31f3affc97c9e8ee3068ed73d214a6426a
Martien Dermawan Tanama <martiendt@gmail.com>
2025-04-08 15:50:33 +0700

    x

--- src/modules/documents/router.ts
+++ src/modules/documents/router.ts
@@ -7,7 +7,7 @@ import { makeController } from '@/express'
 import * as controller from './controllers/index'
 
 const storage = multer.memoryStorage()
-const upload = multer({ storage })
+const upload = multer({ storage, limits: { fileSize: 50 * 1024 * 1024 } }) // 50MB
 
 const makeRouter = async (routerInput: IBaseAppInput): Promise<Router> => {
   const router = Router()
